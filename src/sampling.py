f6fb7af2e6e8fdef21a7033d0044dfbfcb97d2b3
Akie <aya1023lake@gmail.com>
2020-04-16 12:50:01 +0900

    [add] GAN

--- src/sampling.py
+++ src/sampling.py
@@ -2,23 +2,59 @@
 
 import numpy as np
 from sklearn.preprocessing import normalize
+import sys, os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Dense, Activation, Reshape, Input, BatchNormalization, LeakyReLU
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.optimizers import Adam
+
+import matplotlib.pyplot as plt
 
 # To do: add method of gan
-def make_anchors(X_train, nanc, args):
+def make_anchors(X_train, nanc, args, hidden_dim=256, epochs=2000, latent_dim=100):
     '''
     Parameters
     --------------
-    nanc: number of anchor data
-    X_train: training data to get min(max) value of feature
-    anc_type: 'random', 'gan'
+    nanc: int
+        number of anchor data
+    X_train: 
+        training data to generate anchor data
+    anc_type: 'random', 'gan', 'saved'
+        'gan': train GAN from X_train to generate anchor data
+        'saved': load saved weight of anchor generator previously trained by anc_type='gan'
     
     '''
     if args.anc_type == 'random':
         
-        Xanc = np.random.uniform(low=np.min(X_train), high=np.max(X_train), size=(nanc, X_train.shape[1]))
-        Xanc = normalize(Xanc)
+        anc = np.random.uniform(low=np.min(X_train), high=np.max(X_train), size=(nanc, X_train.shape[1]))
+        # anc = normalize(anc)
     
-    return Xanc
+    elif args.anc_type == 'gan':
+
+        gan = GAN(input_shape=X_train.shape[1], hidden_dim=hidden_dim, latent_dim=latent_dim)
+        gan.train(X_train, epochs=epochs, batch_size=32)
+        
+        noise = np.random.normal(0, 1, size=(nanc, latent_dim))
+        anc = gan.generator.predict(noise)
+
+        # save model
+        gan.generator.save_weights('save/models/generator_%sdata_%sepochs_%sanc.h5' % (len(X_train), epochs, nanc) )
+    
+    elif args.anc_type == 'saved':
+
+        noise = np.random.normal(0, 1, size=(nanc, latent_dim))
+        generator = GAN(X_train.shape[1], hidden_dim, latent_dim).generator
+        generator.load_weights('save/models/generator_1200data_2000epochs_500anc.h5')
+        # generator.load_weights('save/models/generator_%sdata_%sepochs_%sanc.h5' % (len(X_train), epochs, nanc) )
+
+        anc = generator.predict(noise)
+    
+    else:
+        raise Exception('Unrecognized type')
+    
+    return anc
 
 def sampling_iid(X_train, num_users, ndat):
     '''Sample I.I.D user data from dataset
@@ -31,7 +67,7 @@ def sampling_iid(X_train, num_users, ndat):
     list_users, all_idx = [], [i for i in range(len(X_train))]
     for i in range(num_users):
         user_idx = set(np.random.choice(all_idx, size=ndat, replace=False))
-        all_idx = list(set(all_idx) - user_idx)
+        all_idx = list(set(all_idx) - user_idx) # remained index
         list_users.append(list(user_idx))
     
     return list_users
@@ -70,3 +106,135 @@ def sampling_noniid(X_train, label_train,  num_users, ndat, nlabel):
         list_users.append(user_idx)
 
     return list_users
+
+
+class GAN():
+    def __init__(self, input_shape, hidden_dim=256, latent_dim=100, optimizer=Adam(0.0002, 0.5)):
+        self.input_shape = (input_shape,)
+        self.hidden_dim = hidden_dim
+        self.latent_dim = latent_dim
+        
+        # define generator instance
+        self.generator = self.build_generator()
+        
+        
+        # defince discriminator instance
+        self.discriminator = self.build_discriminator()
+        self.discriminator.compile(loss='binary_crossentropy', optimizer=optimizer)
+        
+        # defined combined network
+        self.combined = self.build_combined()
+        self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
+        
+    
+    # generator model
+    def build_generator(self):
+        
+        model = Sequential()
+        
+        model.add(Dense(self.hidden_dim, input_shape=(self.latent_dim,))) # 100 -> 256
+        model.add(LeakyReLU(0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(self.hidden_dim*2)) # 256 -> 512
+        model.add(LeakyReLU(0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(self.hidden_dim*4)) # 512 -> 1024
+        model.add(LeakyReLU(0.2))
+        model.add(BatchNormalization(momentum=0.8))
+        model.add(Dense(np.prod(self.input_shape), activation='tanh'))
+#         model.add(Reshape(self.input_shape))
+        
+        model.summary()
+        
+        return model
+    
+    # discriminator model
+    def build_discriminator(self):
+        
+        model = Sequential()
+        
+#         model.add(Flatten(input_shape=self.input_shape))
+#         AttributeError: 'tuple' object has no attribute 'lower'
+        model.add(Dense(self.hidden_dim*2, input_shape=self.input_shape)) # 256 -> 512
+        model.add(LeakyReLU(0.2))
+        model.add(Dense(self.hidden_dim)) # 512 -> 256
+        model.add(LeakyReLU(0.2))
+        model.add(Dense(1, activation='sigmoid')) # 256 -> number of class (0 or 1)
+        
+        model.summary()
+        
+        return model
+    
+    # generatorとdiscriminatorを結合させる
+    
+    def build_combined(self):
+        self.discriminator.trainable = False
+        model = Sequential([self.generator, self.discriminator])
+        return model
+        
+    
+    def train(self, X_train, epochs, batch_size=32):
+        
+        half_batch = int(batch_size / 2)
+        
+        num_batches = int(X_train.shape[0] / half_batch)
+        print('Number of batches:', num_batches)
+        
+        
+        for epoch in range(epochs+1):
+            
+            for iteration in range(num_batches):
+            
+                # ------ discriminator --------
+
+                # pick up half-batch size of datafrom generator: G(x)
+                noise = np.random.normal(0, 1, (half_batch, self.latent_dim))
+                gen_data = self.generator.predict(noise)
+
+                # pick up half-batch size of dataset from dataset: D(x)
+                idx = np.random.choice(X_train.shape[0], half_batch)
+                true_data = X_train[idx]
+
+                # make label for training: gen_data=0, true_data=1
+                y_gen = np.zeros((half_batch, 1))
+                y_true = np.ones((half_batch, 1))
+
+                # learn discriminator
+                d_loss_fake = self.discriminator.train_on_batch(gen_data, y_gen)
+                d_loss_real = self.discriminator.train_on_batch(true_data, y_true)
+
+                # average each loss
+                d_loss = 0.5 * np.add(d_loss_fake, d_loss_real)
+
+                
+                # ------- generator ---------
+
+                noise = np.random.normal(0, 1, (half_batch, self.latent_dim))
+                
+                # make label
+                valid_y = np.ones((half_batch, 1))
+                # valid_y = np.array([1] * half_batch)
+
+                # train generator
+                g_loss = self.combined.train_on_batch(noise, valid_y)
+                
+            # progress
+            print('Epoch: %d, [D loss: %f] [G loss: %f]' % (epoch, d_loss, g_loss))
+
+            if epoch % 200 == 0:
+                self.save_fig(X_train, epoch)
+
+    # generate 25 figures during training
+    def save_fig(self, X_train, epoch):
+        noise = np.random.normal(0, 1, size=(25, self.latent_dim))
+        gen_img = self.generator.predict(noise)
+
+        fig, axs = plt.subplots(5,5, figsize=(5,5))
+
+        for i in range(5):
+            for j in range(5):
+                axs[i, j].imshow(gen_img[i*5+j].reshape(28, 28), cmap='gray')
+        
+        fig.savefig('save/figures/%sdata_%sepoch.png' % (len(X_train), epoch))
+        plt.close()
+        
